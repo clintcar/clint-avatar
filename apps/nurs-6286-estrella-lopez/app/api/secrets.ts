012cff4c04dba72f8dcdd8c28fea22ea98015267
Clint Carlson <clintcarlson@gmail.com>
2026-01-05 15:00:15 -0700

    Update default avatar ID, knowledge base, and headshot

--- apps/nurs-6286-estrella-lopez/app/api/secrets.ts
+++ apps/nurs-6286-estrella-lopez/app/api/secrets.ts
@@ -1,10 +1,10 @@
 export const API_KEY = "2e23fcdc-cef5-11f0-a99e-066a7fa2e369";
 export const API_URL = "https://api.liveavatar.com";
-export const AVATAR_ID = "6e32f90a-f566-45be-9ec7-a5f6999ee606";
+export const AVATAR_ID = "bf00036b-558a-44b5-b2ff-1e3cec0f4ceb";
 
 // FULL MODE Customizations
 // Wayne's avatar voice and context
-export const CONTEXT_ID = "1d8ea5e3-b009-4244-9d81-fc9c434c547d";
+export const CONTEXT_ID = "d95f4b2c-b81b-4fe5-afd3-3d428e724ee6";
 export const LANGUAGE = "en";
 
 // CUSTOM MODE Customizations
